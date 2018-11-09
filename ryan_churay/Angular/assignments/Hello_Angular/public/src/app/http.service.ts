dd0f3c3bd050efbeae92fa1f54bceb6353a51d5d
GrammySpaghetti <ryanchuray@me.com>
2018-11-08 20:47:22 -0600

    api assignment

--- ryan_churay/Angular/assignments/Hello_Angular/public/src/app/http.service.ts
+++ ryan_churay/Angular/assignments/Hello_Angular/public/src/app/http.service.ts
@@ -9,13 +9,32 @@ export class HttpService {
   constructor(private _http: HttpClient) {
     this.getTasks();
     this.taskID();
+    this.trivia();
+    this.get50();
   }
   getTasks(){
-    let tempObservable = this._http.get('/tasks');
-    tempObservable.subscribe(data => console.log('Got our tasks!', data));
+    let tempObservable1 = this._http.get('/tasks');
+    tempObservable1.subscribe(data => console.log('Got our tasks!', data));
   }
   taskID(){
-    let tempObservable = this._http.get('/tasks/5be377c286a517056deb66d8');
-    tempObservable.subscribe(data => console.log('Got the task by ID!', data));
+    let tempObservable2 = this._http.get('/tasks/5be377c286a517056deb66d8');
+    tempObservable2.subscribe(data => console.log('Got the task by ID!', data));
+  }
+  trivia(){
+    let tempObservable3 = this._http.get('https://opentdb.com/api_category.php');
+    tempObservable3.subscribe(data => {
+      console.log('Here are the trivia categories', data);
+      this.triviaCatId(data);
+    });
+  }
+  get50(){
+    let tempObservable4 = this._http.get('https://opentdb.com/api.php?amount=50');
+    tempObservable4.subscribe(data => console.log('50 random trivia questions', data));
+  }
+  triviaCatId(data){
+    let category = data.trivia_categories[0].id;
+    let tempObservable5 = this._http.get(`https://opentdb.com/api.php?amount=10&category=${category}`);
+    console.log(category);
+    tempObservable5.subscribe(data => console.log('category based on id', data));
   }
 }
